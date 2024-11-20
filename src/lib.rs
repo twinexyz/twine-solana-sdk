cf8386a4b4bf845726fa731b11d6127e3678f982
Kobby Pentangeli <kobbypentangeli@gmail.com>
2024-11-20 14:08:06 +0000

    Solana AccountHasher

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,8 @@
 pub mod account;
+pub mod account_hasher;
 pub mod atomic_u64;
 pub mod clock;
 pub mod decode_error;
 pub mod hash;
 pub mod pubkey;
 pub mod sanitize;
-
-/// The unit of time a given leader schedule is honored.
-///
-/// It lasts for some number of [`Slot`]s.
-pub type Epoch = u64;
